8e0aa1bf4169b0228ecd257fde985d486ae5b49e
vincent <mincentvulder@gmail.com>
2020-10-08 18:17:31 +0200

    new try

--- sources/train.py
+++ sources/train.py
@@ -11,60 +11,59 @@ logging.basicConfig(level=logging.INFO)
 # and will make bigger leaps to adjust
 # convergence
 
-def update_thetas(theta_0, theta_1, mileage, price, total, learning_rate):
-    theta_0_deriv = 0
-    theta_1_deriv = 0
-
-    for i in range(total):
-        # Calculate partial derivatives
-        # -2x(y - (mx + b))
-        theta_0_deriv += -2*mileage[i] * (price[i] - (theta_0*mileage[i] + theta_1)) # weight
-
-        # -2(y - (mx + b))
-        theta_1_deriv += -2*(price[i] - (theta_0*mileage[i] + theta_1)) #  bias
-
-    # We subtract because the derivatives point in direction of steepest ascent
-    theta_0 -= (theta_0_deriv / total) * learning_rate
-    theta_1 -= (theta_1_deriv / total) * learning_rate
-
-    return theta_0, theta_1
-
-def cost_function(theta_0, theta_1, mileage, price, total):
-    '''this number represents how wrong the model is in terms 
-    of its ability to estimate the relationship between X and y'''
-    error = 0.0
-    for i in range(total):
-        error += (price[i] - (theta_0 * mileage[i] + theta_1))**2   #MSE 
-    return error / total
-
 def linear_regression(dataset: NDArray[(24, 2), Int[64]]):
-    print("dataset:\n", dataset, "\n")
+    print(dataset)
+    converged = False
+    iter = 0
+    ep = 1
+    max_iter = 100
     learning_rate = 0.1
-    theta_0 = 0.0                                            # the weight
-    theta_1 = 0.0                                            # the bias
+    t0 = 0.01                                            # the weight
+    t1 = 0.01                                            # the bias
     mileage = [x[0] for x in dataset]
     price = [x[1] for x in dataset]
     total = len(dataset)
-    cost_history = []
+    cost = sum([(t0 + t1*mileage[i] - price[i])**2 for i in range(total)])
     # gradient descent
-    for i in range(100):
-        theta_0, theta_1 = update_thetas(theta_0, theta_1, mileage, price, total, learning_rate)
+    while not converged:
+        # for each training sample, compute the gradient (d/d_theta j(theta))
+        grad0 = 1.0/total * sum([abs((t0 + t1*mileage[i] - price[i])) for i in range(total)]) 
+        grad1 = 1.0/total * sum([abs((t0 + t1*mileage[i] - price[i])*mileage[i]) for i in range(total)])
+        # print(grad0)
+        # # print(grad1)
+        # exit()
 
-        #Calculate cost for auditing purposes
-        cost = cost_function(theta_0, theta_1, mileage, price, total)
-        cost_history.append(cost)
+        # update the theta_temp
+        temp0 = t0 - learning_rate * grad0
+        temp1 = t1 - learning_rate * grad1
+    
+        # update theta
+        t0 = temp0
+        t1 = temp1
 
-        # Log Progress
-        if i % 10 == 0:
-            print(f"iter={i}    theta_0={theta_0}    theta_1={theta_1}    cost={cost}")
-    print("Final theta's: ", theta_0, theta_1)
+        # mean squared error
+        new_cost = sum([(t0 + t1*mileage[i] - price[i])**2 for i in range(total)])
+
+        if abs(cost - new_cost) <= ep:
+            print ('Converged, iterations: ', iter, '!!!')
+            converged = True
+    
+        cost = new_cost   # update error 
+        iter += 1         # update iter
+    
+        if iter == max_iter:
+            print ('Max interactions exceeded!')
+            converged = True
+
+        print(f"iter={iter} theta_0={t0} theta_1={t1} cost={cost}")
+    print("Final theta's: ", t0, t1)
 
 
 
 def extract_data():
-    data_file = Path(Path.cwd()/"ndata.csv")
+    data_file = Path(Path.cwd()/"data.csv")
     if data_file.exists():
-        dataset = np.genfromtxt(data_file, delimiter=',', skip_header=1, dtype=float)
+        dataset = np.genfromtxt(data_file, delimiter=',', skip_header=1, dtype=int)
         linear_regression(dataset)
     else:
         logging.error(f"Could not find your dataset, should be stored as such: {Path.cwd()}/ndata.csv")
